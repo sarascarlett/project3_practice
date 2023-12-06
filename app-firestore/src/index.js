4456537f5839dd43b9c0a878ee9193dba0d27683
Sara L Scarlett <sarascarlett1@Saras-MacBook-Air-4.local>
2023-12-06 14:46:15 -0500

    “addedcode”

--- app-firestore/src/index.js
+++ app-firestore/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ClassDetails from './ClassDetails';
 import reportWebVitals from './reportWebVitals';
+
+var rentalInfo = {name: 'Casa Era Ora', pic: "https://a0.muscache.com/im/pictures/3542b90c-d817-44ef-a4df-fef3578b2735.jpg?im_w=960",
+  description: 'Entire home hosted by Marco',
+  size: '6 guests 3 bedrooms 2 baths', id: 'bprxyx' };  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+     <ClassDetails id={rentalInfo.id} name={rentalInfo.name} pic={rentalInfo.pic} 
+      description={rentalInfo.description} size={rentalInfo.size} /> 
+  </React.StrictMode> 
 );
 
 // If you want to start measuring performance in your app, pass a function
