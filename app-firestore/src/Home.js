fea22d6b100d48cc792bcce89cb2491dd98f9e35
Madison Guarin <madisonguarin@Madisons-MacBook-Pro-8.local>
2023-12-06 14:56:03 -0500

    add Home

---
+++ app-firestore/src/Home.js
@@ -0,0 +1,22 @@
+const Home = ({ navigation }) => {
+
+    const dataSource = [
+        { title: "Algorithms and Data Structures I ", courseCode: "CSC 1051" },
+        { title: "The Enrichment Seminar", courseCode: "CSC 1990" },
+        { title: "Algorithms and Data Structures II", courseCode: "CSC 1052" },
+        { title: "Discrete Structures", courseCode: "CSC 1300" },
+        { title: "Computing Systems I ", courseCode: "CSC 2400" },
+        { title: "Platform Based Computing", courseCode: "CSC 2053" },
+        { title: "Statistics for Computing", courseCode: "CSC 2300" },
+        { title: "Computing Systems II", courseCode: "CSC 2405" },
+        { title: "Analysis of Algorithms", courseCode: "CSC 1700" },
+        { title: "Programming Languages", courseCode: "CSC 1800" },
+        { title: "Theory of Computation", courseCode: "CSC 4480" },
+        { title: "Database Systems ", courseCode: "CSC 2405" },
+        { title: "Software Engineering", courseCode: "CSC 4700" },
+        { title: "Senior Project", courseCode: "CSC 4790" },
+    
+      ];
+    }
+    
+    export default Home;
--- app-firestore/src/index.js
+++ app-firestore/src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+var rentalInfo = {name: 'Casa Era Ora', pic: "https://a0.muscache.com/im/pictures/3542b90c-d817-44ef-a4df-fef3578b2735.jpg?im_w=960",
+  description: 'Entire home hosted by Marco',
+  size: '6 guests 3 bedrooms 2 baths', id: 'bprxyx' };  
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+     <RentalDetails id={rentalInfo.id} name={rentalInfo.name} pic={rentalInfo.pic} 
+      description={rentalInfo.description} size={rentalInfo.size} /> 
+  </React.StrictMode> 
 );
 
 // If you want to start measuring performance in your app, pass a function
